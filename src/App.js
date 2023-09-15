462e2df9153306c59ce00d5aa496dce75cbc6cbc
AmyHMSmith <amyhmsmith@gmail.com>
2023-09-15 15:20:31 +0100

    selection page successful

--- src/App.js
+++ src/App.js
@@ -1,28 +1,51 @@
-import './styles/App.css';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Home from './components/Home';
-import Quiz from './components/Quiz';
-import Result from './components/Result';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import "./styles/App.css";
+import { useState } from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import Quiz from "./components/Quiz";
+import Result from "./components/Result";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import axios from "axios";
 
 const App = () => {
+  const [name, setName] = useState("");
+  const [questions, setQuestions] = useState("");
+
+  const fetchQuestions = async (category = "", difficulty = "") => {
+    const { data } = await axios.get(
+      `https://opentdb.com/api.php?amount=10${
+        category && `&category=${category}`
+      }${difficulty && `&difficulty=${difficulty}`}&type=multiple`
+    );
+
+    console.log(data);
+  };
+
   return (
     <BrowserRouter>
-    <div className="App">
-      <Header />
-  
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/quiz" element={<Quiz />} />
-        <Route path="/result" element={<Result />} />
+      <div className="App">
+        <Header />
+
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Home
+                name={name}
+                setName={setName}
+                fetchQuestions={fetchQuestions}
+              />
+            }
+          />
+          <Route path="/quiz" element={<Quiz />} />
+          <Route path="/result" element={<Result />} />
         </Routes>
-        
-        </div>
-      
+      </div>
+
       <Footer />
-      </BrowserRouter>
-    )
-}
+    </BrowserRouter>
+  );
+};
 
 export default App;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,23 +1,56 @@
-import { Button, MenuItem, TextField } from '@material-ui/core';
-import React, { useState } from 'react';
-import '../styles/Home.css';
-import Categories from '../Data/Categories';
+import { Button, MenuItem, TextField } from "@material-ui/core";
+import React, { useState } from "react";
+import "../styles/Home.css";
+import Categories from "../Data/Categories";
+import { useNavigate } from "react-router-dom";
+import ErrorMessage from "./ErrorMessage";
 
-const Home = () => {
-  const [category, setCategory] = useState('');
-  const [difficulty, setDifficulty] = useState('');
-  
+const Home = ({ name, setName, fetchQuestions }) => {
+  const [category, setCategory] = useState("");
+  const [difficulty, setDifficulty] = useState("");
+  const [error, setError] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleSubmit = () => {
+    if (!category || !difficulty || !name) {
+      setError(true);
+      return;
+    } else {
+      setError(false);
+      fetchQuestions(category, difficulty);
+      navigate("/quiz");
+    }
+  };
+
   return (
-    <div className='content'>
-      <div className='settings'>
+    <div className="content">
+      <div className="settings">
         <span>Quiz Settings</span>
 
-        <div className='settings_select'>
-          <TextField style={{ marginBottom: 25 }} label='Enter your name' variant='outlined' />
+        <div className="settings_select">
+          {error && <ErrorMessage>Cannot leave blank</ErrorMessage>}
 
-          <TextField select style={{ marginBottom: 25 }} label='Select category' variant='outlined'>  
+          <TextField
+            style={{ marginBottom: 25 }}
+            label="Enter your name"
+            variant="outlined"
+            onChange={(event) => setName(event.target.value)}
+          />
+
+          <TextField
+            select
+            style={{ marginBottom: 25 }}
+            label="Select category"
+            value={category}
+            onChange={(event) => setCategory(event.target.value)}
+            variant="outlined"
+          >
             {Categories.map((cat) => (
-              <MenuItem key={cat.category} value={cat.value}> {cat.category}</MenuItem>  
+              <MenuItem key={cat.category} value={cat.value}>
+                {" "}
+                {cat.category}
+              </MenuItem>
             ))}
           </TextField>
           <TextField
@@ -25,6 +58,8 @@ const Home = () => {
             label="Select Difficulty"
             variant="outlined"
             style={{ marginBottom: 25 }}
+            value={difficulty}
+            onChange={(event) => setDifficulty(event.target.value)}
           >
             <MenuItem key="Easy" value="easy">
               Easy
@@ -40,17 +75,16 @@ const Home = () => {
             variant="contained"
             color="primary"
             size="large"
+            onClick={handleSubmit}
           >
             Start Quiz
           </Button>
         </div>
-
       </div>
 
-      <img src='quiz.svg' className='banner' alt='quiz img' />
-
+      <img src="quiz.svg" className="banner" alt="quiz img" />
     </div>
-      )
-}
+  );
+};
 
 export default Home;
